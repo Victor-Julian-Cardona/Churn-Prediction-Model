a764c8c95dbea38bcd7e65213f6457078c233dbb
Victor Cardona <victor.cardona5@gmail.com>
2024-07-07 17:34:21 -0400

    data is loaded, load_data has better comments. admin.py now correctly registers the Customer model

--- churn/load_data.py
+++ churn/load_data.py
@@ -6,23 +6,29 @@ import sys
 # Ensure the project root is in the Python path
 sys.path.append('C:/Users/victo/Documents/Churn-Prediction-Model')
 
+# Set the Django settings module environment variable
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'churn_project.settings')
+
+# Initialize Django
 django.setup()
 
+# Import the Customer model from the churn app
 from churn.models import Customer
 
 def load_data():
+    # Read the CSV file into a DataFrame
     data = pd.read_csv('Telco-Customer-Churn.csv')
     
     # Replace spaces with NaN
     data['TotalCharges'] = data['TotalCharges'].replace(" ", pd.NA)
     
-    # Convert to float
-    data['TotalCharges'] = data['TotalCharges'].astype(float)
+    # Convert the TotalCharges column to float after ensuring all values are clean
+    data['TotalCharges'] = pd.to_numeric(data['TotalCharges'], errors='coerce')
     
     # Fill NaN values with the median
     data['TotalCharges'].fillna(data['TotalCharges'].median(), inplace=True)
 
+    # Iterate over the DataFrame and create Customer objects
     for index, row in data.iterrows():
         Customer.objects.create(
             customerID=row['customerID'],
